568ad8782fd6f0a3a44e5e404acf0dfdd710142b
pace11 <ryanjoker87@gmail.com>
2020-10-09 19:29:15 +0700

    feat: improve new env db prod

--- config/connection.php
+++ config/connection.php
@@ -6,10 +6,17 @@
     // $db     ="finance_master";
     // $conn   = mysqli_connect($host, $user, $pass, $db);
 
+    // database canary dev
+    // $host   ="45.33.24.136";
+    // $user   ="pacegaga_pace";
+    // $pass   ="Pace561996";
+    // $db     ="pacegaga_simkeu";
+    // $conn   = mysqli_connect($host, $user, $pass, $db);
+
     // database production
-    $host   ="45.33.24.136";
-    $user   ="pacegaga_pace";
-    $pass   ="Pace561996";
-    $db     ="pacegaga_simkeu";
+    $host   ="103.147.154.52";
+    $user   ="smwdoffi_simkeu";
+    $pass   ="smwd_simkeu2020";
+    $db     ="smwdoffi_simkeu";
     $conn   = mysqli_connect($host, $user, $pass, $db);
 ?>
